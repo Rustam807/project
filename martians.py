35e2d9f2fd01e909e9ade5330e7e5f993336444d
Rustam Mukhamedov <rustam.mukhamedov@alatoo.edu.kg>
2024-11-22 18:52:03 +0600

    deleting exccessing lines

--- martians.py
+++ martians.py
@@ -18,38 +18,36 @@ while mark1 > 7 or mark2 > 7 or mark3 > 7 or mark1 < 0 or mark2 < 0 or mark3 < 0
     mark2 = int(input())
     mark3 = int(input())
 
-if mark1 == km1 and mark2 == km2 and mark3 == km3:
-    print("Martians have succesfully found all 713 kg of cargo")
-else:
-    while mark1 != km1 or mark2 == km2 or mark3 == km3:
-        foundcargo = 0
-        if mark1 == km1:
-            foundcargo += 250
-        if mark2 == km2:
-            foundcargo += 300
-        if mark3 == km3:
-            foundcargo += 163
 
-        print("Martians have found only " + str(foundcargo) + " kg of cargo")
+ while mark1 != km1 or mark2 == km2 or mark3 == km3:
+    foundcargo = 0
+    if mark1 == km1:
+        foundcargo += 250
+    if mark2 == km2:
+        foundcargo += 300
+    if mark3 == km3:
+        foundcargo += 163
 
-        kms = random.sample(range(1, 8), 3)
-        km1, km2, km3 = kms
+    print("Martians have found only " + str(foundcargo) + " kg of cargo")
 
-        cargo1 = [km1, 250]
-        cargo2 = [km2, 300]
-        cargo3 = [km3, 163]
+    kms = random.sample(range(1, 8), 3)
+    km1, km2, km3 = kms
 
-        print("Enter 3 marks")
+    cargo1 = [km1, 250]
+    cargo2 = [km2, 300]
+    cargo3 = [km3, 163]
+
+    print("Enter 3 marks")
+    mark1 = int(input())
+    mark2 = int(input())
+    mark3 = int(input())
+
+    while mark1 > 7 or mark2 > 7 or mark3 > 7 or mark1 < 0 or mark2 < 0 or mark3 < 0:
+        print("It's only 7 km to city. Martians have made a mistake with their marks. They should enter them again")
         mark1 = int(input())
         mark2 = int(input())
         mark3 = int(input())
 
-        while mark1 > 7 or mark2 > 7 or mark3 > 7 or mark1 < 0 or mark2 < 0 or mark3 < 0:
-            print("It's only 7 km to city. Martians have made a mistake with their marks. They should enter them again")
-            mark1 = int(input())
-            mark2 = int(input())
-            mark3 = int(input())
-
-        if mark1 == km1 and mark2 == km2 and mark3 == km3:
-            break
+    if mark1 == km1 and mark2 == km2 and mark3 == km3:
+        break
 print("Martians have succesfully found all 713 kg of cargo")
