4e5829523c77978dbac39f4bcc10d77c7b622e1c
Rustam Mukhamedov <rustam.mukhamedov@alatoo.edu.kg>
2024-11-22 18:24:06 +0600

    While added

--- martians.py
+++ martians.py
@@ -12,9 +12,19 @@ mark2 = int(input())
 mark3 = int(input())
 
 if mark1 == km1 and mark2 == km2 and mark3 == km3:
-    print("Martians have succesfully found all the 713 kg of cargo")
+    print("Martians have succesfully found all 713 kg of cargo")
 else:
-    while mark1 != km1 and mark2 != km2 and mark3 != km3:
+    while mark1 != km1 or mark2 == km2 or mark3 == km3:
+        foundcargo = 0
+        if mark1 == km1:
+            foundcargo += 250
+        if mark2 == km2:
+            foundcargo += 300
+        if mark3 == km3:
+            foundcargo += 163
+
+        print("Martians have found only " + str(foundcargo) + " kg of cargo")
+
         kms = random.sample(range(1, 8), 3)
         km1, km2, km3 = kms
 
@@ -25,3 +35,7 @@ else:
         mark1 = int(input())
         mark2 = int(input())
         mark3 = int(input())
+
+        if mark1 == km1 and mark2 == km2 and mark3 == km3:
+            print("Martians have succesfully found all 713 kg of cargo")
+            break
